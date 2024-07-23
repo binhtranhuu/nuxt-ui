c61820c9c86b5d3fc0e55dbbe716675720c1d9a0
Benjamin Canac <canacb1@gmail.com>
2024-07-23 14:36:58 +0200

    playground(nuxt.config): add `compatibilityDate`

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,15 +3,5 @@ export default defineNuxtConfig({
     '../src/module'
   ],
 
-  compatibilityDate: '2024-07-23',
-
-  vite: {
-    optimizeDeps: {
-      esbuildOptions: {
-        supported: {
-          'top-level-await': true
-        }
-      }
-    }
-  }
+  compatibilityDate: '2024-07-23'
 })
