2998650133efc81fa42d4b8dea7531c4a11bfb40
binhth-1206 <tran.huu.binh@sun-asterisk.com>
2024-08-05 21:48:04 +0700

    chore: improve ui page component

--- src/runtime/ui.config/layout/page.ts
+++ src/runtime/ui.config/layout/page.ts
@@ -2,9 +2,9 @@ export default {
   wrapper: 'flex flex-col lg:grid lg:grid-cols-10 lg:gap-8',
   left: 'order-last lg:col-span-2',
   center: {
-    narrow: 'order-first lg:col-span-6',
-    base: 'order-first lg:col-span-8',
-    full: 'order-first lg:col-span-10'
+    narrow: 'lg:col-span-6',
+    base: 'lg:col-span-8',
+    full: 'lg:col-span-10'
   },
   right: 'lg:col-span-2'
 }
